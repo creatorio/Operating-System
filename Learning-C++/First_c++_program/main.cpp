1dd68c700c5b0044dbff435cf41a2d4dbfecbdf4
creatorio <rdjunior018@gmail.com>
2024-03-11 18:50:08 +0530

    first commit

---
+++ Learning-C++/First_c++_program/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    std::cout << "cwd" << std::endl;
+    return 0;
+}
